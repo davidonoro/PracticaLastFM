baab9ddbcfa29d409f7a105e02ba7a1e2293e067
matinte <miguel.tinte@gmail.com>
2016-02-04 21:08:26 +0100

    Borrador Ejercicio7.sql

---
+++ src/main/resources/ejercicio7.sql
@@ -0,0 +1,24 @@
+-- Usamos rangos edad calculados en ejercicio 3 en agePercentiles
+
+-- Join de los datos de fichero tsv y sql
+hadoop jar target/Practica1-0.0.1-SNAPSHOT.jar practica1.datasetab.join.DriverJoin /user/cloudera/practica1/playbyUserGroup /user/cloudera/practica1/userid /user/cloudera/practica1/joindataB
+
+-- Generacion de fichero de salida agrupado por grupo, rango de edad y sexo 
+hadoop jar target/Practica1-0.0.1-SNAPSHOT.jar practica1.dataseta.ejercicio7.DriverEjercicio7 /user/cloudera/practica1/joindataA /user/cloudera/practica1/agePercentiles/000000_0 /user/cloudera/practica1/ejercicio3
+
+CREATE TABLE ejercicio7 (
+cancion STRING,
+rango STRING,
+plays INT)
+ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t' 
+LOCATION '/user/cloudera/practica1/ejercicio7';
+
+CREATE TABLE ejercicio7Final
+ROW FORMAT DELIMITED FIELDS TERMINATED BY '\t'
+LOCATION '/user/cloudera/practica1/ejercicio7Final' AS
+SELECT cancion, rango, plays
+FROM (
+SELECT e7.cancion, e7.rango, e7.plays, row_number() over (Partition BY e7.cancion, e7.rango order by e7.plays desc) as row
+FROM ejercicio7 e7
+) rs
+WHERE row <= 10;
