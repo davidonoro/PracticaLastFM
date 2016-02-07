855de55bafa39e597e42d594aa5c13b70b470a53
matinte <miguel.tinte@gmail.com>
2016-02-07 18:22:10 +0100

    Update ejercicio7.sql

--- src/main/resources/ejercicio7.sql
+++ src/main/resources/ejercicio7.sql
@@ -1,7 +1,7 @@
 -- Usamos rangos edad calculados en ejercicio 3 en agePercentiles
 
 -- Join de los datos de fichero tsv y sql
-hadoop jar target/Practica1-0.0.1-SNAPSHOT.jar practica1.datasetb.join.DriverJoin /user/cloudera/practica1/tsv/infoPlaysArtistUser /user/cloudera/practica1/sql/userid /user/cloudera/practica1/joindataB
+hadoop jar target/Practica1-0.0.1-SNAPSHOT.jar practica1.datasetb.join.DriverJoin /user/cloudera/practica1/tsv/userid /user/cloudera/practica1/sql/userid /user/cloudera/practica1/joindataB
 
 -- Generacion de fichero de salida agrupado por cancion y rango de edad 
 hadoop jar target/Practica1-0.0.1-SNAPSHOT.jar practica1.datasetb.ejercicio7.DriverEjercicio7 /user/cloudera/practica1/joindataB /user/cloudera/practica1/agePercentiles/000000_0 /user/cloudera/practica1/ejercicio7
