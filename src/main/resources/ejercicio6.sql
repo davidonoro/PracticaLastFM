a8bc49f569d0e7dd37aaf81872e8332fd0641554
juliofrancisco <juliofrancisco@gmail.com>
2016-02-08 21:42:01 +0100

    Para hacer un decode del sexo y saque nulos y blancos a NI

--- src/main/resources/ejercicio6.sql
+++ src/main/resources/ejercicio6.sql
@@ -12,3 +12,19 @@ group by uj.year,uj.weekYear,uj.gender,uj.traname
 ) agregated
 ) agregatedordered
 where agregatedordered.row <= 10;
+
+
+
+--para hacer un decode del sexo 
+CREATE TABLE weekgendertopsongs
+ROW FORMAT DELIMITED FIELDS TERMINATED BY ','
+LOCATION '/user/cloudera/practica1/weekgendertopsongs' AS
+select year, weekYear, gender,traname,numplays from (
+select agregated.*, row_number() over (PARTITION BY agregated.year,agregated.weekYear,agregated.gender order by numplays desc) as row
+from (
+select uj.year,uj.weekYear, if(uj.gender='m','m', if (uj.gender='f','f','NI')) as gender ,uj.traname, count(*) as numplays
+from usertplaystimestamp_userid_join uj
+group by uj.year,uj.weekYear,uj.gender,uj.traname
+) agregated
+) agregatedordered
+where agregatedordered.row <= 10;
